561aa77a7066e1e06e04876cb32af94316b8f367
doug0849 <etaomonk@gmail.com>
2022-06-25 06:33:21 +0800

    feat: init app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
+// const usePassport = require('./config/passport')
+
+// require('./config/mongoose')
+
+const app = express()
+const port = process.env.PORT
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine','hbs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true })) //body-parser
+app.use(methodOverride('_method'))
+
+app.listen(port, () => {
+  console.log('Server is started on http://localhost:3000 ...')
+})
